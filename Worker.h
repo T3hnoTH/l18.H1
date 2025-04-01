d4f658dd6a21d857714706e30823d67460dbf390
hedgirun@gmail.com <hedgirun@gmail.com>
2025-04-01 12:30:16 +0300

    Added function 1

--- Worker.h
+++ Worker.h
@@ -5,14 +5,14 @@ using namespace std;
 
 struct Worker {
     char surname[50] = "undefined";
-    char birthdate[11] = "01.01.1901";
+    int birthyear = 1901;
     char pos[50] = "undefined";
     float salary = 4000;
     char educ[50] = "undefined";
     
     void showInfo(){
         cout << setw(20) << "Surname: " << surname << endl;
-        cout << setw(20) << "Birthdate: " << birthdate << endl;
+        cout << setw(20) << "Birthdate: " << birthyear << endl;
         cout << setw(20) << "Position: " << pos << endl;
         cout << setw(20) << "Salary: " << salary << "UAH." << endl;
         cout << setw(20) << "Education: " << educ << " UAH" << endl;
--- main.cpp
+++ main.cpp
@@ -4,10 +4,15 @@ using namespace std;
 int main(){
     int sizeA = 5;
     Worker* arr = new Worker[sizeA]{
-        {"Olegov", "01.01.1998", "Manager", 11999.99, "Higher education"},
-        {"Karpenko", "10.10.2000", "Programmer", 8999.99, "Higher education"},
-        {"Kovalenko", "12.12.1999", "Designer", 8899.99, "Higher education"},
-        {"But", "01.04.2006", "Tester", 5199.99, "Higher education"},
-        {"Sergeiv", "15.05.2004", "Cleanerr", 4499.99, "Secondary education"}
+        {"Olegov", 1998, "Manager", 11999.99, "Higher education"},
+        {"Karpenko", 2000, "Programmer", 8999.99, "Higher education"},
+        {"Kovalenko", 1999, "Designer", 8899.99, "Higher education"},
+        {"But", 2006, "Tester", 5199.99, "Higher education"},
+        {"Sergeiv", 1960, "Cleaner", 4499.99, "Secondary education"}
     };
+    for (int i = 0; i < sizeA; i++){
+        if (arr[i].birthyear < 1965){
+            arr[i].showInfo();
+        }
+    }
 }
